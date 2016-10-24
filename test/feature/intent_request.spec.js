c4bcbe10e73751d8095f04ca59293340e6fcac0c
Matthew Clark <matt@notch8.com>
2016-10-24 03:23:05 +0000

    adds ability to keep session open and prompt user if they want more events

--- test/feature/intent_request.spec.js
+++ test/feature/intent_request.spec.js
@@ -36,76 +36,76 @@ after( (done) => {
 });
 
 
-describe("WhatsHappening 3", ()=> {
+describe("Event Requests", ()=>{
   var timekeeper = require('timekeeper');
-  var threeEventTime = new Date("2016-10-23T01:48:54+00:00");
-  var sevenEventTime = new Date("2016-10-26T01:48:54+00:00");
 
-  describe("3 events", ()=>{
-
-    var mockRequest = mockHelper.load("whats_happening_request.json");
-
-    it("should end session", ()=>{
-      timekeeper.freeze(threeEventTime);
-      return app.request(mockRequest).then( (response)=>{
-        var subject = response.response;
-        expect(subject).to.have.property("shouldEndSession", true);
-       });
-    });
-
-    describe("response", ()=>{
-      it("should say the correct events", ()=>{
-        timekeeper.freeze(threeEventTime);
-        const expected = '<speak>There are 3 events today. At 08:00:00 AM, PDX Women In Tech (PDXWIT) Coderetreat 2016. At 08:00:00 AM, PDX Global Day of Coderetreat 2016. At 10:00:00 AM, DevelopmentNow Little Hackathon of Horrors.</speak>'
-
-        return app.request(mockRequest).then( (response)=>{
-          var subject = response.response.outputSpeech
-          expect(subject.ssml).to.equal(expected);
-        });
-      });
-
-      it("should add a card", ()=>{
-        const expected = 'There are 3 events today.\nPDX Women In Tech (PDXWIT) Coderetreat 2016\nStarts At: 8:00 AM\n310 SW 4th Ave Suite 412 Portland, OR 97204\nPDX Global Day of Coderetreat 2016\nStarts At: 8:00 AM\n249 NW Park Ave Portland, OR 97209\nDevelopmentNow Little Hackathon of Horrors\nStarts At: 10:00 AM\n9 SE 3rd Ave Ste 220, Portland, OR 97214'
-
-        return app.request(mockRequest).then( (response)=>{
-          var subject = response.response.card
-          expect(subject.content).to.equal(expected);
-        });
-      });
-    });
+  describe("MoreEvents", ()=>{
+    var sevenEventTime = new Date("2016-10-26T01:48:54+00:00");
   });
 
-  describe("more than events", ()=>{
-
+  describe("WhatsHappening", ()=> {
+    var threeEventTime = new Date("2016-10-23T01:48:54+00:00");
+    var sevenEventTime = new Date("2016-10-26T01:48:54+00:00");
     var mockRequest = mockHelper.load("whats_happening_request.json");
 
-    it("should end session", ()=>{
-      timekeeper.freeze(sevenEventTime);
-      return app.request(mockRequest).then( (response)=>{
-        var subject = response.response;
-        expect(subject).to.have.property("shouldEndSession", false);
-       });
-    });
-
-    describe("response", ()=>{
-      it("should reprompt", ()=>{
-        timekeeper.freeze(sevenEventTime);
-        const expected = '<speak>Would you like to hear more?</speak>';
+    describe("3 events", ()=>{
+      it("should end session", ()=>{
+        timekeeper.freeze(threeEventTime);
         return app.request(mockRequest).then( (response)=>{
-          var subject = response.response.reprompt.outputSpeech
-          expect(subject.ssml).to.equal(expected);
-        });
+          var subject = response.response;
+          expect(subject).to.have.property("shouldEndSession", true);
+         });
       });
 
-      it("should set targetDate in session", ()=>{
+      describe("response", ()=>{
+        it("should say the correct events", ()=>{
+          timekeeper.freeze(threeEventTime);
+          const expected = '<speak>There are 3 events today. At 08:00:00 AM, PDX Women In Tech (PDXWIT) Coderetreat 2016. At 08:00:00 AM, PDX Global Day of Coderetreat 2016. At 10:00:00 AM, DevelopmentNow Little Hackathon of Horrors.</speak>'
+
+          return app.request(mockRequest).then( (response)=>{
+            var subject = response.response.outputSpeech
+            expect(subject.ssml).to.equal(expected);
+          });
+        });
+
+        it("should add a card", ()=>{
+          const expected = 'There are 3 events today.\nPDX Women In Tech (PDXWIT) Coderetreat 2016\nStarts At: 8:00 AM\n310 SW 4th Ave Suite 412 Portland, OR 97204\nPDX Global Day of Coderetreat 2016\nStarts At: 8:00 AM\n249 NW Park Ave Portland, OR 97209\nDevelopmentNow Little Hackathon of Horrors\nStarts At: 10:00 AM\n9 SE 3rd Ave Ste 220, Portland, OR 97214'
+
+          return app.request(mockRequest).then( (response)=>{
+            var subject = response.response.card
+            expect(subject.content).to.equal(expected);
+          });
+        });
+      });
+    });
+
+    describe("more than events", ()=>{
+      it("should end session", ()=>{
         timekeeper.freeze(sevenEventTime);
         return app.request(mockRequest).then( (response)=>{
-          var subject = response.sessionAttributes
-          expect(subject.relativeTargetDay).to.equal('today');
+          var subject = response.response;
+          expect(subject).to.have.property("shouldEndSession", false);
+         });
+      });
+
+      describe("response", ()=>{
+        it("should reprompt", ()=>{
+          timekeeper.freeze(sevenEventTime);
+          const expected = '<speak>Would you like to hear more?</speak>';
+          return app.request(mockRequest).then( (response)=>{
+            var subject = response.response.reprompt.outputSpeech
+            expect(subject.ssml).to.equal(expected);
+          });
+        });
+
+        it("should set targetDate in session", ()=>{
+          timekeeper.freeze(sevenEventTime);
+          return app.request(mockRequest).then( (response)=>{
+            var subject = response.sessionAttributes
+            expect(subject.relativeTargetDay).to.equal('today');
+          });
         });
       });
     });
   });
 });
-
-
